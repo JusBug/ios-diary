907a1705d9d1c69b76857f5381e64bc5ecfc7679
JusBug <pksgwo123@gmail.com>
2023-09-01 21:02:14 +0900

    feat: CoreDataManager 타입 및 Persistent container 생성

---
+++ Diary/Model/CoreDataManager.swift
@@ -0,0 +1,24 @@
+//
+//  CoreDataManager.swift
+//  Diary
+//
+//  Created by 박종화 on 2023/09/01.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    static var shared: CoreDataManager = CoreDataManager()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Diary")
+        
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            guard let error = error as NSError? else {
+                return
+            }
+        })
+        return container
+    }()
+}
